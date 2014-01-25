36476a5dd467f80f6c7c38b6f10cae65bdd326b3
Andrew Brockert <andrew@mercuric.net>
2014-01-25 18:40:53 -0500

    cleaned some meat off the skeleton

--- web/src/game.js
+++ web/src/game.js
@@ -2,7 +2,7 @@ Game = {
   // Initialize and start our game
   start: function() {
     // Start crafty and set a background color so that we can see it's working
-    Crafty.init(800, 500);
-    Crafty.background('green');
+    Crafty.init(false, false);
+    Crafty.background('black');
   }
 }
