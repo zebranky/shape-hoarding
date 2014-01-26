6959629d40250cc6a63caeaea9a5e670ec661d28
Andrew Dolce <andrew.dolce@gmail.com>
2014-01-25 20:39:03 -0500

    Experiment with draggable objects

--- web/src/game.js
+++ web/src/game.js
@@ -2,7 +2,62 @@ Game = {
   // Initialize and start our game
   start: function() {
     // Start crafty and set a background color so that we can see it's working
-    Crafty.init(800, 500);
+
+    var WIDTH = 800;
+    var HEIGHT = 500;
+
+    Crafty.init( WIDTH, HEIGHT );
     Crafty.background('green');
+
+    var ent = Crafty.e('2D, DOM, Color, Draggable').attr({
+      x: 0, y: 0, w: 100, h: 100
+    }).color('#F00');
+
+    var margin = 5;
+
+    var snapEntity = function( ent, width, height, margin ) {
+      if ( ent.x < margin ) {
+        ent.x = margin;
+      } else if ( ent.x + ent.w > width - margin ) {
+        ent.x = width - margin - ent.w;
+      }
+
+      if ( ent.y < margin ) {
+        ent.y = margin;
+      } else if ( ent.y + ent.h > height - margin ) {
+        ent.y = height - margin - ent.h;
+      }
+    };
+
+    var count = 0;
+    ent.bind( 'Dragging', function( mouseevent ) {
+      snapEntity( ent, WIDTH, HEIGHT, margin );
+    });
+
+
+    var pointInBounds = function( x, y, w, h ) {
+      return ( x > 0 ) &&
+        ( x < w ) &&
+        ( y > 0 ) &&
+        ( y < h );
+    };
+
+    Crafty.stage.elem.addEventListener( 'mouseleave', function( event ) {
+      if ( event.target === Crafty.stage.elem ) {
+        ent.stopDrag();
+      }
+    });
+
+    // ent.bind( 'MouseOut', function() {
+    //   ent.stopDrag();
+    // });
+
+    // Crafty.bind( 'Click', function() {
+    //   console.log( 'click' );
+    // });
+
+    // Crafty.stage.bind( 'MouseOut', function() {
+    //   console.log( 'out of stage' );
+    // });
   }
 }
