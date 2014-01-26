ec26551c72988b3f53bac5d46829342c8c50dfc6
ahorne <spam4aaron@gmail.com>
2014-01-26 11:41:50 -0500

    Added a variety of clip art images

--- web/src/game.js
+++ web/src/game.js
@@ -4,5 +4,11 @@ Game = {
     // Start crafty and set a background color so that we can see it's working
     Crafty.init(false, false);
     Crafty.background('black');
+    
+    var images = 
+    [
+    	'one.png',
+    	'two.png'
+    ];
   }
-}
+};
